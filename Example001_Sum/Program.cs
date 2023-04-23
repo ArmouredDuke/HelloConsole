86458a26fa3995a2e8e02c80af7c299a2611ab97
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-04-23 16:19:11 +0300

    добавили рандом и вывод

--- Example001_Sum/Program.cs
+++ Example001_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA = 31;
-int numB = 5;
+﻿int numA = new Random().Next(1, 10); // new Random().Next(min, max-1) в данном случае будет числовой ряд от 1 до 9, ибо int
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
 int result = numA + numB;
 Console.WriteLine(result);
