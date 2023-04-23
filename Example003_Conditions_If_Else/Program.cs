916efbee64bd70e62e927c6c320620c9e3a679f5
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-04-23 16:37:39 +0300

    бета-маша

---
+++ Example003_Conditions_If_Else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя в пользователя: ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+    Console.WriteLine("Где тебя черти носили, сиповка привокзальная?!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
